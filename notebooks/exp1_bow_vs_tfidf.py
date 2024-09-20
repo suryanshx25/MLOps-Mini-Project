0b72ba25d272bfa632d15e9fd73b766ed623b963
Suryansh Singh <suryanshx25@gmail.com>
2024-09-20 13:12:58 +0530

    multiple changes

--- notebooks/exp1_bow_vs_tfidf.py
+++ notebooks/exp1_bow_vs_tfidf.py
@@ -19,9 +19,8 @@ import numpy as np
 import os
 
 import dagshub
-
-mlflow.set_tracking_uri('https://dagshub.com/campusx-official/mlops-mini-project.mlflow')
-dagshub.init(repo_owner='campusx-official', repo_name='mlops-mini-project', mlflow=True)
+dagshub.init(repo_owner='suryanshx25', repo_name='MLOps-Mini-Project', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/suryanshx25/MLOps-Mini-Project.mlflow')
 
 # Load the data
 df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv').drop(columns=['tweet_id'])
--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -16,8 +16,8 @@ import numpy as np
 import os
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/campusx-official/mlops-mini-project.mlflow')
-dagshub.init(repo_owner='campusx-official', repo_name='mlops-mini-project', mlflow=True)
+dagshub.init(repo_owner='suryanshx25', repo_name='MLOps-Mini-Project', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/suryanshx25/MLOps-Mini-Project.mlflow')
 
 # Load the data
 # Load the data
